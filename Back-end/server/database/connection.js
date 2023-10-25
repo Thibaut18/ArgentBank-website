081b1ab922597ce59ee2388e4ccf951a3651470f
Teinturier Thibaut <thibaut.teinturier18@gmail.com>
2023-10-25 10:28:11 +0200

    Ready To Front

---
+++ Back-end/server/database/connection.js
@@ -0,0 +1,14 @@
+/* Modifs pour pouvoir lancer le serveur */
+
+const mongoose = require('mongoose')
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://127.0.0.1/argentBankDB'
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    console.log('Database successfully connected')
+  } catch (error) {
+    console.error(`Database Connectivity Error: ${error}`)
+    throw new Error(error)
+  }
+}
